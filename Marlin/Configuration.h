40217b0ab1fccdbfdb958ab5a1db86f0531dba6e
Anthony Vanden Bossche <toonevdb@gmail.com>
2019-03-07 20:11:14 +0100

    baudrate 25k, disabled lcd menus

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -116,7 +116,7 @@
  *
  * :[2400, 9600, 19200, 38400, 57600, 115200, 250000, 500000, 1000000]
  */
-#define BAUDRATE 115200
+#define BAUDRATE 250000
 
 // Enable the Bluetooth serial interface on AT90USB devices
 //#define BLUETOOTH
@@ -1448,7 +1448,7 @@
  * Disable all menus and only display the Status Screen, or
  * just remove some extraneous menu items to recover space.
  */
-//#define NO_LCD_MENUS
+#define NO_LCD_MENUS
 //#define SLIM_LCD_MENUS
 
 //
